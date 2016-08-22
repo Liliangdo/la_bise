80a140cbf704ddefd73210d306f5f7ecd4f183c0
lilian garrido <lilian.garrido13@gmail.com>
2016-08-22 18:15:16 +0200

    create model Message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822161228) do
+ActiveRecord::Schema.define(version: 20160822161443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 20160822161228) do
     t.index ["user_id"], name: "index_events_on_user_id", using: :btree
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_messages_on_event_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -49,4 +59,6 @@ ActiveRecord::Schema.define(version: 20160822161228) do
   end
 
   add_foreign_key "events", "users"
+  add_foreign_key "messages", "events"
+  add_foreign_key "messages", "users"
 end
