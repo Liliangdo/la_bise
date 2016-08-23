770a6a0c99dfb1070a68a5f548e6c3699de49b28
lilian garrido <lilian.garrido13@gmail.com>
2016-08-23 15:18:01 +0200

    finish the controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-
+  skip_before_action :authenticate_user!, only: [:index, :show, :new]
   before_action :find_event, only: [:show, :create, :edit, :update]
 
   def index
@@ -14,13 +14,19 @@ class EventsController < ApplicationController
   end
 
   def create
-
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to dashboard_path
+    else
+      render :new
   end
 
   def edit
   end
 
   def update
+    @event.update(event_params)
+    redirect_to dashboard_path
   end
 
   private
@@ -30,13 +36,13 @@ class EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:events).permit( 
+    params.require(:events).permit(
       :name,
       :description,
       :price,
       :house_type,
       :capacity,
-      :starting_at, 
+      :starting_at,
       photos: [] )
   end
 end
