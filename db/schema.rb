e844da9ca0af5f000aa04a8f00798d97fbb3e827
Laurianne Gerin <laurianne.gerin@gmail.com>
2016-08-23 17:29:29 +0200

    Completed a little more the Home view, added the variable events in pages controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160823101035) do
+ActiveRecord::Schema.define(version: 20160823141349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,11 @@ ActiveRecord::Schema.define(version: 20160823101035) do
     t.string   "gender"
     t.string   "phone_number"
     t.text     "description"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
