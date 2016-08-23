5e8bf43ff094b9eb9488a0e8455a028d64d6c59f
DESROSIERS <mdesrosiers.pro@gmail.com>
2016-08-23 14:35:06 +0200

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require_tree .
+
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+//= require_tree .
