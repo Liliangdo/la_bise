353a7c474b44d726332edc306f1197975e004246
lilian garrido <lilian.garrido13@gmail.com>
2016-08-23 13:44:37 +0200

    add private method for controller Users and Events plus attachinary params for both of them

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,4 +16,23 @@ class EventsController < ApplicationController
 
   def update
   end
+
+  private
+
+  def find_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:events).permit( photos: [],
+      :name,
+      :description,
+      :price,
+      :house_type,
+      :capacity,
+      :starting_at )
+  end
 end
+
+
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
+
+  before_action :find_user, only: [:show, :edit]
+
   def show
+
   end
 
   def edit
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:users).permit(:photo)
   end
 end
