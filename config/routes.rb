21a3288e1784ec35c60d3999b7bf43b258728074
lilian garrido <lilian.garrido13@gmail.com>
2016-08-23 11:59:11 +0200

    create the roads

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :events, only: [:index, :show, :new, :create, :edit, :update] do
+    resources :messages, only: [:index, :new, :create]
+    resources :reviews, only: [:index, :create]
+  end
+  resources :reservations, only: [:index, :new, :create, :update, :destroy]
+  resources :users, only: [:show, :edit, :update]
 end
+
