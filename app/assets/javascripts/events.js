5aae0ab41cd3c84d340cfad860cc72582c7cccd1
Laurianne Gerin <laurianne.gerin@gmail.com>
2016-08-24 14:46:59 +0200

    I tried but no success

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,4 +1,15 @@
 $('.date').datepicker({
   toggleActive: true,
   todayHighlight: true,
+  language: 'fr',
+});
+
+
+$('#iconified').on('keyup', function() {
+    var input = $(this);
+    if(input.val().length === 0) {
+        input.addClass('empty');
+    } else {
+        input.removeClass('empty');
+    }
 });
