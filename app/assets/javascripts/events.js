a45580a562e58d1affcd9e8940e99c53b506c857
Laurianne Gerin <laurianne.gerin@gmail.com>
2016-08-24 16:16:33 +0200

    Update events.js

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,15 +1,5 @@
 $('.date').datepicker({
   toggleActive: true,
   todayHighlight: true,
-  language: 'fr',
 });
 
-
-$('#iconified').on('keyup', function() {
-    var input = $(this);
-    if(input.val().length === 0) {
-        input.addClass('empty');
-    } else {
-        input.removeClass('empty');
-    }
-});
