cfdc285aeafa396d81ef674e99d693bac9ece55c
Laurianne Gerin <laurianne.gerin@gmail.com>
2016-08-24 14:58:26 +0200

    A continuer

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,4 +1,15 @@
 $('.date').datepicker({
   toggleActive: true,
   todayHighlight: true,
+  language: 'fr'
 });
+
+
+$.fn.datetimepicker.dates['fr'] = {
+    days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
+    daysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"],
+    daysMin: ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa", "Di"],
+    months: ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"],
+    monthsShort: ["Jan", "Fev", "Mar", "Avr", "Mai", "Jun", "Jul", "Aou", "Sep", "Oct", "Nov", "Dec"],
+    today: "Aujourd'hui"
+};
