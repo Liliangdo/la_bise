43d5fbd5d756fa0ec7f6222f98d57c74a205b0bf
Laurianne Gerin <laurianne.gerin@gmail.com>
2016-08-24 10:49:59 +0200

    Some requirements in application.css and application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,8 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 
+//= require bootstrap-datepicker
+
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
