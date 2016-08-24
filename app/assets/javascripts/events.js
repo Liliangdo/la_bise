a66bda4de8c5c4030ba2e416d5fcd2cdf49620d4
Laurianne Gerin <laurianne.gerin@gmail.com>
2016-08-24 17:51:53 +0200

    Changement de la langue du date picker

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,16 +1,17 @@
-$('.date').datepicker({
-  toggleActive: true,
-  todayHighlight: true,
-  language: 'fr'
-});
-
-
 $.fn.datepicker.dates['fr'] = {
     days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
     daysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"],
     daysMin: ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa", "Di"],
     months: ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"],
     monthsShort: ["Jan", "Fev", "Mar", "Avr", "Mai", "Jun", "Jul", "Aou", "Sep", "Oct", "Nov", "Dec"],
-    today: "Aujourd'hui"
+    today: "Aujourd'hui",
+    format: "dd/mm/yyyy",
 };
+
+
+$('.date').datepicker({
+  toggleActive: true,
+  todayHighlight: true,
+  language: $(".date").data("lang")
+});
 
