73c123bc0502cb95d9015fd72beac4e8dea90381
marc desrosiers <mdesrosiers@norauto.fr>
2016-08-29 22:02:18 +0200

    solved the problem with attachinary

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def helper_picture_url(event)
     if event.photos?
-      cl_image_path(event.photos.path)
+      cl_image_path(event.photos.first.path)
     else
       "http://www.ledarlington.com/wp-content/uploads/2015/02/happyhour.jpg"
     end
@@ -9,7 +9,7 @@ module ApplicationHelper
 
   def helper_picture_tag(event)
     if event.photos?
-      cl_image_tag(event.photos.path)
+      cl_image_tag(event.photos.first.path)
     else
       image_tag("image-cover.jpg")
     end
