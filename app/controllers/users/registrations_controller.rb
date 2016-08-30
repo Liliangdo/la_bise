875f5ca81ac8f3757f110549bf8ca5db3ec3d90e
lilian garrido <lilian.garrido13@gmail.com>
2016-08-30 14:38:05 +0200

    fixed the forms edit

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
 
+
   def after_update_path_for(resource)
     if session[:retake_create_event]
       session.delete(:retake_create_event)
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,14 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:facebook]
 
   has_many :reservations
-  has_many :events, through: :reservations
+
+  has_many :events
+  has_many :participed_events, through: :reservations, source: :event
+
   has_many :messages_sent, class_name: "Message", foreign_key: :sender_id
   has_many :messages_received, class_name: "Message", foreign_key: :recipient_id
+
+  has_many :given_reviews, through: :participed_events, source: :reviews
   has_many :reviews, through: :events
 
 
