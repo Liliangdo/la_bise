8a14cfc4da98fee3223782505df19b8ec04dc77c
Sebastien Carnelos <sebastien.carnelos@gmail.com>
2016-08-31 20:28:04 +0200

    Give up messages 'cause it looks so bad

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -26,12 +26,12 @@ class MessagesController < ApplicationController
   end
 
   def edit
-    message_load
     @message.update
   end
 
   def update
     @message.update(message_params)
+    message_load
     authorize @message
     redirect_to dashboard_path
   end
@@ -48,5 +48,6 @@ class MessagesController < ApplicationController
 
   def message_load
     @message.content = "#{@message.content}\n#{'-' * 3} \n#{User.find(@message.sender_id).username} - #{l Time.now, format: :short}\n#{'*' * 50}\n"
+    authorize @message
   end
 end
