5cf886edc4539d638b211e194777ee709e6de667
marc desrosiers <mdesrosiers@norauto.fr>
2016-09-01 08:15:14 +0200

    a solution to have flexible date... Gift

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831104315) do
+ActiveRecord::Schema.define(version: 20160901052527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20160831104315) do
     t.string   "address"
     t.string   "sku"
     t.integer  "price_cents", default: 0, null: false
+    t.boolean  "flexible"
     t.index ["user_id"], name: "index_events_on_user_id", using: :btree
   end
 
